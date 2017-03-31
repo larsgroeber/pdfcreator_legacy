aacbdb4809912ee690eddaff62914db9973fdf71
Lars Gröber <larsgroeber7@gmail.com>
2017-03-31 23:59:47 +0200

    added https support

--- config.example.ts
+++ config.example.ts
@@ -5,7 +5,7 @@ export const ROOT_URL = '/';
 // Root url for the express server, might be different form ROOT_URL e.g. if behind a proxy
 export const ROOT_URL_EXPRESS = '/';
 
-// Port on which the server should listen
+// Http Port on which the server should listen, leave empty (0) to disable http and only use https
 export const PORT = 3000;
 
 // Path to the folder where you want to save assets needed to compile latex documents
@@ -13,3 +13,11 @@ export const DATA_PATH = '/home/lars/Documents/programming/angular2Test/pdfcreat
 
 // URL to the backend
 export const SERVER_URL = 'http://localhost:3000';
+
+////// SSL //////
+// Paths to private key and certificate, leave empty if you do not need https
+export const PRIVATE_KEY = '';
+export const CERTIFICATE = '';
+
+// Https Port
+export const PORT_SSL = 3001;
--- server/server.ts
+++ server/server.ts
@@ -11,6 +11,8 @@ import express = require('express');
 import path = require('path');
 import bodyParser = require('body-parser');
 import router = require('./router');
+import fs = require('fs');
+import https = require('https');
 
 import * as Config from '../config';
 
@@ -23,10 +25,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(Config.ROOT_URL_EXPRESS, express.static(path.join(__dirname, '../dist/')));
 
-const port = Config.PORT || 3000;
+if (Config.PORT) {
+  const port = Config.PORT || 3000;
 
-app.set('port', port);
+  app.set('port', port);
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Listening for http requests on port ${port}`);
+  });
+}
+
+if (Config.PORT_SSL && Config.PRIVATE_KEY && Config.CERTIFICATE) {
+  const port = Config.PORT_SSL || 3001;
+
+  try {
+    let privateKey = fs.readFileSync(Config.PRIVATE_KEY, 'utf8');
+    let certificate = fs.readFileSync(Config.CERTIFICATE, 'utf8');
+    let credentials = {key: privateKey, cert: certificate};
+
+    let httpsServer = https.createServer(credentials, app);
+    httpsServer.listen(port, () => {
+      console.log(`Listening for https requests on port ${port}`);
+    })
+  } catch (err) {
+    console.log(`There was an error setting up the https server: ${err}`);
+  }
+}
